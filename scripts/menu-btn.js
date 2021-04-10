b191286c38f731ae8ef24272e1a54239ccc71c7b
ThomAlmeidaRD <thomas.almeidard@gmail.com>
2021-04-10 10:30:41 -0300

    BTN MENU JS

---
+++ scripts/menu-btn.js
@@ -0,0 +1,11 @@
+function menu() {
+
+    var x = document.getElementById("menu")
+
+    if (x.style.display == "inline-block") {
+        x.style.display = "none"
+    } else {
+        x.style.display = "inline-block"
+    }
+
+}
